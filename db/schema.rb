875ee356242740d1398494268c6e15c30150c5fb
kota.kashiwase <kota.kashiwase@d2c.co.jp>
2018-04-25 08:30:35 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20180425055732) do
 
-  create_table "tasks", force: :cascade do |t|
+  create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.         "title"
     t.string   "status"
   end
 
